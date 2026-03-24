5da1bdc5c932a55c85ba30d6cfcd28aaa67fb98e
diegofregoso30 <jrdiego110@gmail.com>
2026-03-23 21:03:23 -0600

    Creacion de la pantalla principal con estilos.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Programa de materia: Desarrollo de Apps Web y Moviles
 La venganza del bote
 
 bote de basura
+
+- 23 de marzo del 2026; Buenas noches, mañana hablamos. by Diego Fregoso
---
+++ src/server/configs/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 840,
+        minWidth: 800,
+        minHeight: 600,
+        title: 'Sistema de Horarios Docentes',
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        }
+    });
+
+    const vistaLogin = path.join(__dirname, '..', '..', 'main', 'interfaces', 'index.html'); mainWindow.loadFile(vistaLogin);
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
